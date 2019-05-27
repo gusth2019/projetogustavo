f382799e0d9a40fdd56cf7b489b9b0778f6d7d97
Gusth2019 <gustavodear22@gmail.com>
2019-05-27 12:46:20 -0300

    azure

---
+++ BD/pasta-projeto-node-site/app-banco.js
@@ -0,0 +1,45 @@
+var desenvolvimento = false;
+
+var configuracoes = {
+    producao: {
+        server: "banco01191117.database.windows.net",
+        user: "Gustavo01191117",
+        password: "#Gf50422207802",
+        database: "Banco01191117",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    },
+    desenvolvimento: {
+        server: "BASETESTE.database.windows.net",
+        user: "usuariotestes",
+        password: "senhatestes",
+        database: "BASETESTE",
+        options: {
+            encrypt: false
+        }
+    }
+}
+ 
+var sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+var perfil = desenvolvimento ? 'desenvolvimento' : 'producao';
+
+function conectar() {
+  return sql.connect(configuracoes[perfil])
+  // return new sql.ConnectionPool();  
+} 
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
