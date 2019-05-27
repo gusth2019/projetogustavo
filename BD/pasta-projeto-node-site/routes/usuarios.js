ba257e63feffd1c81e420e4967df18463aa91e6a
Aluno <Aluno@Digital.net>
2019-05-27 15:00:14 -0300

    conexao completa

--- BD/pasta-projeto-node-site/routes/usuarios.js
+++ BD/pasta-projeto-node-site/routes/usuarios.js
@@ -36,5 +36,53 @@ router.post('/entrar', function (req, res, next) {
 
 });
 
+
+
+
+
+
+
+router.post('/cadastro', function (req, res, next) {
+
+  banco.conectar().then(() => {
+    console.log(`Chegou p/ login: ${JSON.stringify(req.body)}`);
+    var nome = req.body.nome; // depois de .body, use o nome (name) do campo em seu formulário de login
+    var rg = req.body.rg;
+    var cpf = req.body.cpf; // depois de .body, use o nome (name) do campo em seu formulário de login
+    var login = req.body.login;
+    var senha = req.body.senha;
+    if (login == undefined || senha == undefined || nome == undefined || rg == undefined || cpf == undefined ) {
+      throw new Error(`Dados de login não chegaram completos: ${login} / ${senha} / ${nome} / ${rg} / ${cpf}`);
+    }
+    return banco.sql.query(`insert into  Clientes values  ('${nome}', '${rg}', '${cpf}', '${login}', '${senha}')`);
+  }).then(consulta => {
+
+    console.log(`Logins encontrados: ${JSON.stringify(consulta.recordset)}`);
+
+   res.sendStatus(201)
+
+  }).catch(err => {
+
+    var erro = `Erro no login: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 // não mexa nesta linha!
 module.exports = router;
